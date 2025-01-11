2375daabb8b0edd48ac87fdb3b239920051f7e4d
motsan2560 <112244112+Motsan-Git@users.noreply.github.com>
2025-01-11 10:50:21 +0330

    add LongestCommonPrefix

---
+++ app/src/main/kotlin/solutions/LongestCommonPrefix.kt
@@ -0,0 +1,63 @@
+package org.example.app.solutions
+
+/*
+* Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ */
+class LongestCommonPrefix {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        if (strs.isEmpty()) return ""
+
+        var prefix = ""
+
+        for (i in 0 until strs[0].length) {
+            var lookingFor = strs[0][i]
+            for (str in strs) {
+                if (i >= str.length || str[i] != lookingFor)
+                    return prefix
+            }
+            prefix += lookingFor
+        }
+        return prefix
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
