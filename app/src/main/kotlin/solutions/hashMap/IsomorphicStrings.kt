841330167cd4ba242614bd2ed65a769371605f47
motsan2560 <112244112+Motsan-Git@users.noreply.github.com>
2025-01-15 11:43:26 +0330

    Add IsomorphicStrings

---
+++ app/src/main/kotlin/solutions/hashMap/IsomorphicStrings.kt
@@ -0,0 +1,62 @@
+package org.example.app.solutions.hashMap
+
+/*
+* Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+
+
+Example 1:
+
+Input: s = "egg", t = "add"
+
+Output: true
+
+Explanation:
+
+The strings s and t can be made identical by:
+
+Mapping 'e' to 'a'.
+Mapping 'g' to 'd'.
+Example 2:
+
+Input: s = "foo", t = "bar"
+
+Output: false*/
+// مفهوم ایزومورفیسم در اینجا به این معنی است که می‌توان کاراکترهای یک رشته (مثلاً (s)
+// را به صورت یک به یک با کاراکترهای رشته دیگر (مثلاً t) جایگزین کرد،
+// به طوری که ترتیب و ساختار رشته اولیه حفظ شود.
+
+
+class IsomorphicStrings {
+    fun isIsomorphic(s: String, t: String): Boolean {
+
+        return true
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
